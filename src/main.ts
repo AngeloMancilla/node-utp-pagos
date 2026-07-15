ac9528a5ec604d6b6040c613aefe3099b9446029
Angelo Mancilla Mendoza <angelo.mancilla.dev@gmail.com>
2026-07-15 01:59:25 -0500

    feat: :sparkles: feature/UP-5-observabilidad

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpMetricsInterceptor } from './infrastructure/metrics/http-metrics.interceptor';
+import { MetricsService } from './infrastructure/metrics/metrics.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
   });
+  app.useGlobalInterceptors(
+    new HttpMetricsInterceptor(app.get(MetricsService)),
+  );
   await app.listen(process.env.PORT ?? 4000);
 }
 
